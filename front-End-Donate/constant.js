8f16f7c296c62dd465f42713740347ea8b0df18d
Olaoye Salem <89555234+olaoyesalem@users.noreply.github.com>
2023-04-26 04:58:51 +0100

    Create constant.js

--- front-End-Donate/constant.js
+++ front-End-Donate/constant.js
@@ -5,7 +5,6 @@ export const donateAddress = '0x4f7fbc2c1b5A7043aFCe973c7b9dC48D0CBCC878'
 //  export const donateAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
 
 
-
 export const donateFactoryABI =[
   {
     "inputs": [],
