8a8f9c95f45b73399185542efa885083a58a0b52
Olaoye Salem <89555234+olaoyesalem@users.noreply.github.com>
2023-05-05 20:15:40 +1300

    Update Donate.sol

--- contracts/Donate.sol
+++ contracts/Donate.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.7;
 
-
 // Add price converter to make the minimum paymentt $1
 error DonateFactory__NameHasBeenTaken();
 error DonateFactory__NotOwner();
